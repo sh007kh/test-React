43e565d671c135e4c3314f987f3e24d2635650e4
shahab khodayar <sh007kh@gmail.com>
2024-07-28 12:24:51 +0330

    cocktails boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-import "./App.css";
-import Form from "./components/Form";
-function App() {
-  return <Form />;
-}
-
+const App = () => {
+  return <h2>mixmaster starter</h2>;
+};
 export default App;
